9ac6eae127b05d75f997a0f07cfc89b6fe9bcdf4
Constanza-Mardones <61568167+Constanza-Mardones@users.noreply.github.com>
2020-07-24 14:12:14 -0400

    trabajos

---
+++ directivas-vue/var.js
@@ -0,0 +1,10 @@
+const contenido = new Vue({
+	el:'#contenido',
+	data: {
+		message:"en el futuro",
+		uno:true,
+		dos:true,
+		lista: ['Otros Idiomas', 'Técnicas de Dibujo', 'Otras ramas de Diseño Gráfico'],
+		hobbies: ['Tocar Instrumentos', 'Tiro con Arco', 'Orfebrería']
+	}
+})
