6a79b7e626a5d8ce38d4692f31eef724a6f189fa
Constanza-Mardones <61568167+Constanza-Mardones@users.noreply.github.com>
2020-07-27 16:48:45 -0400

    nueva tarea

---
+++ directivas-vue-cli/src/components/Nuevo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="hello">
+    <h1> Lista de cosas por aprender</h1>
+      <ul>
+        <h3>Hobbies</h3>
+        <li>Tocar Instrumentos</li>
+        <li>Tiro con Arco</li>
+        <li>Orfebrería</li>
+      </ul>
+  </div>
+</template>
+<script>
+export default {
+    name:'Nuevo'
+}
+</script>
