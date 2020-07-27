23f2fb477276c9d5e0d64235652f1e7f4b2a79cf
Constanza-Mardones <61568167+Constanza-Mardones@users.noreply.github.com>
2020-07-27 19:21:37 -0400

    tarea componentes

---
+++ multiples-componentes/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="https://s3.amazonaws.com/tinycards/image/3e1981962fc8640613f87ed88fd217cb" width="200">
+    <HelloWorld msg="Lista de Elementos"/>
+    <h2>Subtitulo Lista...</h2>
+    <lista></lista>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Lista from './components/Lista.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Lista
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ multiples-componentes/src/components/Lista.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<ul>
+			<li> <a href="https://www.google.com/search?client=firefox-b-d&bih=626&biw=1366&hl=es-419&sxsrf=ALeKk02RTytylaGIMBvefJfPtIMn5lM8kw%3A1595890994682&ei=Ml0fX6qmKaGP0AaqnYSYAQ&q=definicion+de+hola&oq=definicion+de+hola&gs_lcp=CgZwc3ktYWIQAzIMCAAQFBCHAhBGEPkBMgQIABAKMgQIABAKMgQIABAKMgQIABAKMgcIABAUEIcCMgIIADIECAAQCjICCAAyBAgAEAo6BAgAEEc6CQgjECcQRhD5AToECAAQQzoHCAAQRhD5AVCpEljiGGDOGmgAcAF4AIABgQGIAeADkgEDNC4xmAEAoAEBqgEHZ3dzLXdpesABAQ&sclient=psy-ab&ved=0ahUKEwiqh9qexe7qAhWhB9QKHaoOARMQ4dUDCAs&uact=5">hola</a></li>
+			<li> <a href="https://www.google.com/search?client=firefox-b-d&bih=626&biw=1366&hl=es-419&sxsrf=ALeKk02RTytylaGIMBvefJfPtIMn5lM8kw%3A1595890994682&ei=Ml0fX6qmKaGP0AaqnYSYAQ&q=definicion+de+hola&oq=definicion+de+hola&gs_lcp=CgZwc3ktYWIQAzIMCAAQFBCHAhBGEPkBMgQIABAKMgQIABAKMgQIABAKMgQIABAKMgcIABAUEIcCMgIIADIECAAQCjICCAAyBAgAEAo6BAgAEEc6CQgjECcQRhD5AToECAAQQzoHCAAQRhD5AVCpEljiGGDOGmgAcAF4AIABgQGIAeADkgEDNC4xmAEAoAEBqgEHZ3dzLXdpesABAQ&sclient=psy-ab&ved=0ahUKEwiqh9qexe7qAhWhB9QKHaoOARMQ4dUDCAs&uact=5">has Click aqui</a></li>
+			<li> <a href="https://www.google.com/search?client=firefox-b-d&bih=626&biw=1366&hl=es-419&sxsrf=ALeKk02RTytylaGIMBvefJfPtIMn5lM8kw%3A1595890994682&ei=Ml0fX6qmKaGP0AaqnYSYAQ&q=definicion+de+hola&oq=definicion+de+hola&gs_lcp=CgZwc3ktYWIQAzIMCAAQFBCHAhBGEPkBMgQIABAKMgQIABAKMgQIABAKMgQIABAKMgcIABAUEIcCMgIIADIECAAQCjICCAAyBAgAEAo6BAgAEEc6CQgjECcQRhD5AToECAAQQzoHCAAQRhD5AVCpEljiGGDOGmgAcAF4AIABgQGIAeADkgEDNC4xmAEAoAEBqgEHZ3dzLXdpesABAQ&sclient=psy-ab&ved=0ahUKEwiqh9qexe7qAhWhB9QKHaoOARMQ4dUDCAs&uact=5">Sigue haciendo Click...</a></li>
+			<li> <a href="https://www.google.com/search?client=firefox-b-d&bih=626&biw=1366&hl=es-419&sxsrf=ALeKk02RTytylaGIMBvefJfPtIMn5lM8kw%3A1595890994682&ei=Ml0fX6qmKaGP0AaqnYSYAQ&q=definicion+de+hola&oq=definicion+de+hola&gs_lcp=CgZwc3ktYWIQAzIMCAAQFBCHAhBGEPkBMgQIABAKMgQIABAKMgQIABAKMgQIABAKMgcIABAUEIcCMgIIADIECAAQCjICCAAyBAgAEAo6BAgAEEc6CQgjECcQRhD5AToECAAQQzoHCAAQRhD5AVCpEljiGGDOGmgAcAF4AIABgQGIAeADkgEDNC4xmAEAoAEBqgEHZ3dzLXdpesABAQ&sclient=psy-ab&ved=0ahUKEwiqh9qexe7qAhWhB9QKHaoOARMQ4dUDCAs&uact=5">Si quieres continuar...</a></li>
+			
+		</ul>
+		<ul>
+			<li> <a href="https://www.google.com/search?client=firefox-b-d&bih=626&biw=1366&hl=es-419&sxsrf=ALeKk02RTytylaGIMBvefJfPtIMn5lM8kw%3A1595890994682&ei=Ml0fX6qmKaGP0AaqnYSYAQ&q=definicion+de+hola&oq=definicion+de+hola&gs_lcp=CgZwc3ktYWIQAzIMCAAQFBCHAhBGEPkBMgQIABAKMgQIABAKMgQIABAKMgQIABAKMgcIABAUEIcCMgIIADIECAAQCjICCAAyBAgAEAo6BAgAEEc6CQgjECcQRhD5AToECAAQQzoHCAAQRhD5AVCpEljiGGDOGmgAcAF4AIABgQGIAeADkgEDNC4xmAEAoAEBqgEHZ3dzLXdpesABAQ&sclient=psy-ab&ved=0ahUKEwiqh9qexe7qAhWhB9QKHaoOARMQ4dUDCAs&uact=5">Otros enlaces...</a></li>
+			<li> <a href="https://www.google.com/search?client=firefox-b-d&bih=626&biw=1366&hl=es-419&sxsrf=ALeKk02RTytylaGIMBvefJfPtIMn5lM8kw%3A1595890994682&ei=Ml0fX6qmKaGP0AaqnYSYAQ&q=definicion+de+hola&oq=definicion+de+hola&gs_lcp=CgZwc3ktYWIQAzIMCAAQFBCHAhBGEPkBMgQIABAKMgQIABAKMgQIABAKMgQIABAKMgcIABAUEIcCMgIIADIECAAQCjICCAAyBAgAEAo6BAgAEEc6CQgjECcQRhD5AToECAAQQzoHCAAQRhD5AVCpEljiGGDOGmgAcAF4AIABgQGIAeADkgEDNC4xmAEAoAEBqgEHZ3dzLXdpesABAQ&sclient=psy-ab&ved=0ahUKEwiqh9qexe7qAhWhB9QKHaoOARMQ4dUDCAs&uact=5">Más enlaces...</a></li>
+			<li> <a href="https://www.google.com/search?client=firefox-b-d&bih=626&biw=1366&hl=es-419&sxsrf=ALeKk02RTytylaGIMBvefJfPtIMn5lM8kw%3A1595890994682&ei=Ml0fX6qmKaGP0AaqnYSYAQ&q=definicion+de+hola&oq=definicion+de+hola&gs_lcp=CgZwc3ktYWIQAzIMCAAQFBCHAhBGEPkBMgQIABAKMgQIABAKMgQIABAKMgQIABAKMgcIABAUEIcCMgIIADIECAAQCjICCAAyBAgAEAo6BAgAEEc6CQgjECcQRhD5AToECAAQQzoHCAAQRhD5AVCpEljiGGDOGmgAcAF4AIABgQGIAeADkgEDNC4xmAEAoAEBqgEHZ3dzLXdpesABAQ&sclient=psy-ab&ved=0ahUKEwiqh9qexe7qAhWhB9QKHaoOARMQ4dUDCAs&uact=5">Son todos iguales, deja de hacer click!!!</a></li>
+		</ul>
+	</div>
+</template>
+<script>
+	export default{
+		name:'Lista'		
+	}
+</script>
