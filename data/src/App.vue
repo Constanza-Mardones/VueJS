5d3bc4685ff506311bfd034bf401a75db9a2b9bd
Constanza-Mardones <61568167+Constanza-Mardones@users.noreply.github.com>
2020-07-29 18:06:34 -0400

    nueva tarea

---
+++ data/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld titulo="Tech"/>
+    <HelloWorld v-for="(content, index) in messages" :msg="content" :key="index"/>
+    <HelloWorld titulo="Financial"/>
+    <HelloWorld v-for="(content, index) in messages2" :msg="content" :key="index"/>
+  </div>
+</template>
+
+<script>
+ import HelloWorld from './components/HelloWorld.vue';
+ export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return{
+      messages: ['HTML', 'CSS','Vue'],
+      messages2: ['Investing', 'Budgeting','Balancing a checkbook']
+    };
+  }
+ };
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ data/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h3>{{ titulo }}</h3>
+    <li>{{ msg }}</li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+    props: {
+      titulo:String,
+      msg: Array
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 20px 0 0;
+}
+a {
+  color: #42b983;
+}
+</style>
