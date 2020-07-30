3dc64df81bf58adba70c09d8257f239375c05c14
Constanza-Mardones <61568167+Constanza-Mardones@users.noreply.github.com>
2020-07-29 20:49:46 -0400

    nueva tarea

---
+++ formulario/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Formulario"/>
+    <MiFormulario></MiFormulario>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MiFormulario from './components/MiFormulario.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MiFormulario
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ formulario/src/components/MiFormulario.vue
@@ -0,0 +1,36 @@
+<template>
+  <form action="" method="POST">
+    <p>Nombre: <input type="text" v-model="nombre" ></p>
+    <p>Ciudad:
+        <select v-model="categoria" >
+            <option value="1">Temuco</option>
+            <option value="2">Osorno</option>
+            <option value="3">Santiago</option>
+            <option value="4">Valdivia</option>
+        </select>
+    <p>Lenguaje Favorito:
+        <select v-model="categoria1" >
+            <option value="1">JavaScript</option>
+            <option value="2">Java</option>
+            <option value="3">Phyton</option>
+            <option value="4">Postre</option>
+        </select>
+    </p>
+    <p>Descripción: <textarea v-model="desc" ></textarea></p>
+    <p><input type="submit" value="enviar"></p>
+ </form>
+</template>
+
+<script>
+export default {
+        name: "MiFormulario",
+        data: function() {
+            return {
+                nombre: '',
+                precio: 0,
+                categoria: 2,
+                desc: 'Descripción por defecto'
+            };
+        }
+    }
+</script>
