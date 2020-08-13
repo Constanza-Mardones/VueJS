d236d953f22d51d56b1889d57fb3bbb64e75b078
Constanza-Mardones <61568167+Constanza-Mardones@users.noreply.github.com>
2020-08-13 17:13:20 -0400

    tarea contador vuex

---
+++ contador-vuex/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Counter Vuex"/>
+    <Counter/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Counter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color:rgb(66, 66, 66);
+  width: 1000 px;
+  height: 400px;
+  margin-top: 20px;
+}
+html,
+body{
+  margin:0px;
+  padding: 0px;
+}
+</style>
+
---
+++ contador-vuex/src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="hello">
+      <p>{{ count }}</p>
+      <button v-on:click="respuestaClick()" v-on:mouseover="otroEvento()">Click Me!</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Counter',
+    computed: {
+      count() {
+        return this.$store.state.count
+      }
+    },
+    methods: {
+      respuestaClick() {
+        this.$store.commit('increment')
+      },
+      otroEvento() {
+        this.$store.commit('increment')
+      }
+    }
+  }
+</script>
