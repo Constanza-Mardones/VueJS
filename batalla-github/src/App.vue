28b40684f2cde023d80a943ad0d825e192f987f2
Constanza-Mardones <61568167+Constanza-Mardones@users.noreply.github.com>
2020-08-25 21:07:28 -0400

    github battle

---
+++ batalla-github/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="https://www.sferalabs.cc/wp-content/uploads/github-logo-white.png" width="200">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  margin-top: 60px;
+}
+html
+body{
+  background-color: black;
+}
+</style>
---
+++ batalla-github/src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="hello">
+    <h1>Welcome to GitHub Battle</h1>
+    <div class='cuadro'>
+      <h2>Player 1</h2>
+      <p>Username: {{ info.login }}</p>
+      <p>Public Repositories: {{ info.public_repos }}</p>
+      <p>Number of Followers: {{ info.followers }}</p>
+      <p>Number of Following: {{ info.following }}</p>
+      <p>Public Gists: {{ info.public_gists }}</p>
+      <h3>Total Score: {{  info.public_repos + info.followers + info.following + info.public_gists}}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      info: null
+    };
+  },
+  mounted() {
+    this.getBerryData();
+  },
+  methods: {
+    async getBerryData() {
+      try {
+        let response = await this.$http.get("https://api.github.com/users/Constanza-Mardones");
+        this.info = response.data;      // a lot more information comes back in the response, so for now we only want the actual data of the response
+      }
+      catch(error) {
+        console.log(`Something went wrong: ${error}`);
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.cuadro{
+  width: 300px;
+  height: 260px;
+  color: black;
+  background-color: white;
+  display: inline-block;
+  vertical-align: center;
+  border-radius: 10px;
+  font-size: 15px;
+}
+</style>
