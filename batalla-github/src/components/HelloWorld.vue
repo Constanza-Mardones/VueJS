7b63398ad504f9f652d0685473686e8e6ce6a52f
Constanza-Mardones <61568167+Constanza-Mardones@users.noreply.github.com>
2020-08-27 17:46:52 -0400

    Update HelloWorld.vue

--- batalla-github/src/components/HelloWorld.vue
+++ batalla-github/src/components/HelloWorld.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="hello">
     <h1>Welcome to GitHub Battle</h1>
-    <div class='cuadro'>
+    <p>Look up by id: <input v-model="nombre" placeholder="nombre de usuario"/><button @click="updateResults">Buscar</button></p>
+    <div v-if="info != null" class='cuadro'>
       <h2>Player 1</h2>
       <p>Username: {{ info.login }}</p>
       <p>Public Repositories: {{ info.public_repos }}</p>
@@ -18,17 +19,21 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      info: null
+      info: null,
+      nombre: '',
     };
   },
   mounted() {
     this.getBerryData();
   },
   methods: {
+    updateResults() {
+      this.getBerryData();
+    },
     async getBerryData() {
       try {
-        let response = await this.$http.get("https://api.github.com/users/Constanza-Mardones");
-        this.info = response.data;      // a lot more information comes back in the response, so for now we only want the actual data of the response
+        let response = await this.$http.get(`https://api.github.com/users/${this.nombre}`);  // instead of hardcoding a value, let's use whatever has been typed in the input box
+        this.info = response.data;
       }
       catch(error) {
         console.log(`Something went wrong: ${error}`);
